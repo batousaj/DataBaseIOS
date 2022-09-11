f34d037b152a0c18b8246e93183d87110f5f53ad
thienvu <batousaj@github.com>
2022-09-11 19:54:38 +0700

    Add empty UI

---
+++ SQliteIOS/SQliteIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SQliteIOS
+//
+//  Created by Thien Vu on 09/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
